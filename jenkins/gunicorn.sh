dbcefae76a3ad0680e3ceb9af2e72856bd3e43a3
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-10-09 23:01:54 +0530

    d

--- jenkins/gunicorn.sh
+++ jenkins/gunicorn.sh
@@ -1,13 +1,13 @@
 echo "###################Gunicorn Process Start #####################"
-echo $PWD
-echo "Requeriments Install By Pip Start"
+# echo $PWD
+# echo "Requeriments Install By Pip Start"
 pip install -r app/requirements.txt
 # python3 app/manage.py makemigrations
 # python3 app/manage.py migrate
-python3 app/seed.py
+# python3 app/seed.py
 python3 app/manage.py collectstatic --no-input
-sudo cp -rf jenkins/gunicorn/ecom.socket /etc/systemd/system/
-sudo cp -rf jenkins/gunicorn/ecom.service /etc/systemd/system/
+# sudo cp -rf jenkins/gunicorn/ecom.socket /etc/systemd/system/
+# sudo cp -rf jenkins/gunicorn/ecom.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable ecom
 sudo systemctl start ecom
